e8495a375c362cbb07fa6fe40835ba5a83673e70
thetomek <tommac.opole@gmail.com>
2021-02-18 22:23:34 +0100

    Added missing required attribute

--- LibraryApi/src/Application/Common/BaseDto.cs
+++ LibraryApi/src/Application/Common/BaseDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Application.Common
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Common
 {
     public abstract class BaseDto
     {
+        [Required]
         public int Id { get; set; }
     }
 }
